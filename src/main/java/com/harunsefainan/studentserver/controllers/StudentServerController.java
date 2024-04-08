5bc673875e69e3db144fef858192e89e0cddfd93
harunsefainan <harunsefa.inan@gmail.com>
2024-04-09 02:16:15 +0300

    create StudentServerController

---
+++ src/main/java/com/harunsefainan/studentserver/controllers/StudentServerController.java
@@ -0,0 +1,12 @@
+package com.harunsefainan.studentserver.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/v1/api/students")
+public class StudentServerController {
+
+}
