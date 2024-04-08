164d2493c9d0d080a34332bdd10ba36a4619e0c0
harunsefainan <harunsefa.inan@gmail.com>
2024-04-08 15:49:03 +0300

    update

--- src/main/java/com/harunsefainan/studentserver/repositories/IStudentServerRepository.java
+++ src/main/java/com/harunsefainan/studentserver/repositories/IStudentServerRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IStudentServerRepository extends JpaRepository<StudentServerEntity,String> {
+public interface IStudentServerRepository extends JpaRepository<StudentServerEntity, String> {
 
     IStudentServerRepository getByOid(String oid);
 }
