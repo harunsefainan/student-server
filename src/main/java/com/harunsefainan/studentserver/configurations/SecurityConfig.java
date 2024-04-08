2dbb958b1d77232f18293b80390cd2051c30a5f9
harunsefainan <harunsefa.inan@gmail.com>
2024-04-09 02:11:05 +0300

    update

--- src/main/java/com/harunsefainan/studentserver/configurations/SecurityConfig.java
+++ src/main/java/com/harunsefainan/studentserver/configurations/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.harunsefainan.studentserver.configs;
+package com.harunsefainan.studentserver.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
