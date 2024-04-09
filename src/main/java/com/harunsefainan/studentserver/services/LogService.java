89f398669b2fe8caa63067acc6d70525c0e2c18a
harunsefainan <harunsefa.inan@gmail.com>
2024-04-09 12:33:14 +0300

    create LogService

---
+++ src/main/java/com/harunsefainan/studentserver/services/LogService.java
@@ -0,0 +1,53 @@
+package com.harunsefainan.studentserver.services;
+
+import com.harunsefainan.studentserver.models.StudentServerModel;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogService {
+
+    @Value("${projectName}")
+    private String projectName;
+
+    @Value("${logMsgFormat}")
+    private String logMsgFormat;
+
+    @Value("${logMsgFormat_Ex}")
+    private String logMsgFormatEx;
+
+    final Logger logger = LoggerFactory.getLogger(LogService.class);
+
+    public Boolean generateLog(String uuid, String serviceName, Boolean success, String msj) {
+        logger.error(
+                logMsgFormatEx,
+                uuid,
+                projectName,
+                serviceName,
+                success,
+                msj.replaceAll("\\r\\n|\\r|\\n", " ").trim()
+        );
+        return true;
+    }
+
+    public Boolean generateLog(StudentServerModel user, String uuid, String serviceName, Boolean success, String msj) {
+
+        if (user != null) {
+            logger.error(
+                    logMsgFormatEx,
+                    uuid,
+                    projectName,
+                    user,
+                    serviceName,
+                    success,
+                    msj.replaceAll("\\r\\n|\\r|\\n", " ").trim()
+            );
+            return true;
+        }
+        return false;
+    }
+}
